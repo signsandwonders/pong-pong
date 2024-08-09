764bc77b43c87d8aafb979213c1e5237fa2d212a
signsandwonders <73362951+signsandwonders@users.noreply.github.com>
2024-08-09 13:53:39 -0600

    created basic ball movement loop

--- main.ts
+++ main.ts
@@ -22,17 +22,23 @@ input.onButtonPressed(Button.B, function () {
 function plotBall () {
     if (ball[0] == -1) {
         ball = [randint(0, 4), 0]
+    } else {
+        pball = ball
+        ball = [ball[0], ball[1] + 1]
     }
+    led.unplot(pball[0], pball[1])
     led.plot(ball[0], ball[1])
 }
+let pball: number[] = []
 let ball: number[] = []
 let paddle = 0
 music.play(music.stringPlayable("C D E F G A E C5 ", 352), music.PlaybackMode.UntilDone)
 paddle = 0
 ball = [-1, -1]
-let pball = [-1, -1]
+pball = [-1, -1]
 plotPaddle()
 plotBall()
 basic.forever(function () {
-	
+    basic.pause(1000)
+    plotBall()
 })
