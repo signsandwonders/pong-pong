54a638d357fb48a4673c7e4a87c6328861632f3c
signsandwonders <73362951+signsandwonders@users.noreply.github.com>
2024-08-09 14:31:16 -0600

    basic ball drop game complete

--- main.ts
+++ main.ts
@@ -22,6 +22,16 @@ input.onButtonPressed(Button.B, function () {
 function plotBall () {
     if (ball[0] == -1) {
         ball = [randint(0, 4), 0]
+    } else if (ball[1] == 4) {
+        if (ball[0] == paddle || ball[0] == paddle + 1) {
+            basic.showIcon(IconNames.Yes)
+            basic.clearScreen()
+            reset()
+        } else {
+            basic.showIcon(IconNames.No)
+            basic.clearScreen()
+            reset()
+        }
     } else {
         pball = ball
         ball = [ball[0], ball[1] + 1]
@@ -29,15 +39,18 @@ function plotBall () {
     led.unplot(pball[0], pball[1])
     led.plot(ball[0], ball[1])
 }
+function reset () {
+    music.play(music.stringPlayable("C D E F G A E C5 ", 352), music.PlaybackMode.UntilDone)
+    paddle = 0
+    ball = [-1, -1]
+    pball = [-1, -1]
+    plotPaddle()
+    plotBall()
+}
 let pball: number[] = []
 let ball: number[] = []
 let paddle = 0
-music.play(music.stringPlayable("C D E F G A E C5 ", 352), music.PlaybackMode.UntilDone)
-paddle = 0
-ball = [-1, -1]
-pball = [-1, -1]
-plotPaddle()
-plotBall()
+reset()
 basic.forever(function () {
     basic.pause(1000)
     plotBall()
